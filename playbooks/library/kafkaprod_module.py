1b125635962577a7b7cc58be9f027cc1f897c992
boblongmore <bob.longmore@wwt.com>
2024-04-16 16:11:12 -0400

    added module for producing kafka message

--- playbooks/library/kafkaprod_module.py
+++ playbooks/library/kafkaprod_module.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
 # Copyright: (c) 2018, Terry Jones <terry.jones@example.org>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see COPYING or
+# https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
@@ -58,7 +59,8 @@ EXAMPLES = r'''
 '''
 
 RETURN = r'''
-# These are examples of possible return values, and in general should use other names for return values.
+# These are examples of possible return values, and in general should use
+# other names for return values.
 original_message:
     description: The original name param that was passed in.
     type: str
@@ -72,9 +74,9 @@ message:
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-from kafka import KafkaProducer
-from kafka.errors import KafkaError
 import json
+import asyncio
+from aiokafka import AIOKafkaProducer
 
 
 def run_module():
@@ -84,10 +86,8 @@ def run_module():
         port=dict(type='int', required=True),
         verify_mode=dict(type='bool', required=False, default=True),
         topic=dict(type='str', required=True),
-        group_id=dict(type='str', required=True)
-        data=dict(type='str', required=False,)
-
-
+        group_id=dict(type='str', required=False),
+        data=dict(type='str', required=False)
     )
 
     # seed the result dict in the object
@@ -110,6 +110,33 @@ def run_module():
         supports_check_mode=True
     )
 
+    # produce kafka message
+    host = module.params['host']
+    port = module.params['port']
+    topic = module.params['topic']
+    data = module.params['data']
+
+    def serializer(value):
+        return json.dumps(value).encode()
+
+    async def produce():
+        producer = AIOKafkaProducer(
+            bootstrap_servers=f'{host}:{port}',
+            value_serializer=serializer,
+            compression_type="gzip")
+
+        await producer.start()
+        k_data = data
+        await producer.send(topic, k_data)
+        await producer.stop()
+
+    asyncio.run(produce())
+
+    # producer = KafkaProducer(bootstrap_servers=[f'{host}:{port}'])
+#
+    # producer = KafkaProducer(value_serializer=lambda m: json.dumps(m).encode('ascii'))
+    # producer.send(topic, {data})
+
     # if the user is working with this module in only check mode we do not
     # want to make any changes to the environment, just return the current
     # state with no modifications
@@ -118,19 +145,19 @@ def run_module():
 
     # manipulate or modify the state as needed (this is going to be the
     # part where your module will do what it needs to do)
-    result['original_message'] = module.params['name']
-    result['message'] = 'goodbye'
+    # result['original_message'] = module.params['name']
+    # result['message'] = 'goodbye'
 
     # use whatever logic you need to determine whether or not this module
     # made any modifications to your target
-    if module.params['new']:
-        result['changed'] = True
+    # if module.params['new']:
+    #     result['changed'] = True
 
     # during the execution of the module, if there is an exception or a
     # conditional state that effectively causes a failure, run
     # AnsibleModule.fail_json() to pass in the message and the result
-    if module.params['name'] == 'fail me':
-        module.fail_json(msg='You requested this to fail', **result)
+    # if module.params['name'] == 'fail me':
+    #     module.fail_json(msg='You requested this to fail', **result)
 
     # in the event of a successful module execution, you will want to
     # simple AnsibleModule.exit_json(), passing the key/value results
